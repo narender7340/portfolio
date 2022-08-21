d317f7d47cd00d4c02bed924723dd71a8ce91fce
Narender Kumar Dhaka <87444077+narender7340@users.noreply.github.com>
2022-08-21 18:27:38 +0530

    Update Topbar.jsx

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -7,15 +7,15 @@ export default function Topbar({ menuOpen, setMenuOpen }) {
       <div className="wrapper">
         <div className="left">
           <a href="#intro" className="logo">
-            genius.
+            OOOO.
           </a>
           <div className="itemContainer">
             <Person className="icon" />
-            <span>+44 924 12 74</span>
+            <span>+91 7340****95</span>
           </div>
           <div className="itemContainer">
             <Mail className="icon" />
-            <span>safak@genius.com</span>
+            <span>narender@iitk.ac.in</span>
           </div>
         </div>
         <div className="right">
