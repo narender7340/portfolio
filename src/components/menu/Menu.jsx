59a098f01fafdef2e5dd31c5b79d2602ea92c3b1
Narender Kumar Dhaka <87444077+narender7340@users.noreply.github.com>
2022-12-09 10:34:29 +0530

    Update Menu.jsx

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -8,7 +8,7 @@ export default function Menu({ menuOpen, setMenuOpen }) {
           <a href="#intro">Home</a>
         </li>
         <li onClick={()=>setMenuOpen(false)}>
-          <a href="#portfolio">Project/a>
+          <a href="#portfolio">Project</a>
         </li>
         <li onClick={()=>setMenuOpen(false)}>
           <a href="#works">Works</a>
